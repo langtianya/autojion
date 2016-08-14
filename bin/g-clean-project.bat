0e43fd1b3d4e6b55990dcea16f11339868806ea5
ocq <hansongjy@gmail.com>
2016-08-14 23:28:38 +0800

    modify gradle script and move file

--- bin/g-clean-project.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@echo ------   clean with gradle  ----
-call set-envi.bat
-@echo ------  start  ------------------------
-@REM /c 执行完相应的命令后自动退出命令提示符
-cmd /c gradle clean
-@echo ------  done  -------------------------
-@pause
